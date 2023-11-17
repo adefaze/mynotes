7c3baf046c911205c69943405253451efde75479
adefaze <adefaze@gmail.com>
2023-11-17 19:31:11 +0100

    added auth provider

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/material.dart';
+
+@immutable
+class AuthUser {
+  final bool isemailVerified;
+  const AuthUser(this.isemailVerified);
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+}
