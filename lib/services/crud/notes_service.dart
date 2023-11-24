3728e95d5364031ce017b169916167fe4818f069
adefaze <adefaze@gmail.com>
2023-11-25 00:24:08 +0100

    added database

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -7,8 +7,164 @@ class DatabaseAlreadyOpenException implements Exception {}
 
 class UnableToGetDocumentsDirectory implements Exception {}
 
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotGetNote implements Exception {}
+
 class NotesService {
   Database? _db;
+
+  //  GET ALL NOTES
+  Future<Iterable<DatabaseNote>> getAllNotes() async {
+    final db = _getDatabaseOrThrow();
+    final notes = await db.query(noteTable);
+
+    return notes.map((noteRow) => DatabaseNote.fromRow(noteRow));
+  }
+
+  // GET NOTE
+  Future<DatabaseNote> getNote({required int id}) async {
+    final db = _getDatabaseOrThrow();
+    final note = await db.query(
+      noteTable,
+      limit: 1,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (note.isEmpty) {
+      throw CouldNotGetNote();
+    } else {
+      return DatabaseNote.fromRow(note.first);
+    }
+  }
+
+  // DELETE ALL NOTES
+  Future<int> deleteAllNotes() async {
+    final db = _getDatabaseOrThrow();
+    return await db.delete(noteTable);
+  }
+
+  // DELETE NOTE
+  Future<void> deleteNote({required int id}) async {
+    final db = _getDatabaseOrThrow();
+    final deleteCount = await db.delete(
+      noteTable,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deleteCount == 0) {
+      throw CouldNotDeleteNote();
+    }
+  }
+
+  // CREATE NOTE
+  Future<DatabaseNote> createNote({required DatabaseUser owner}) async {
+    final db = _getDatabaseOrThrow();
+
+    // ENSURE OWNER EXISTS IN THE DB WITH THE CORRECT ID
+
+    final dbUser = await getUser(email: owner.email);
+    if (dbUser != owner) {
+      throw CouldNotFindUser();
+    }
+
+    const text = '';
+    // CREATE THE NOTE
+    final noteId = await db.insert(noteTable, {
+      userIdColumn: owner.id,
+      textColumn: text,
+      isSynchedWithCloudColumn: 1
+    });
+
+    final note = DatabaseNote(
+      id: noteId,
+      userId: owner.id,
+      text: text,
+      isSyncedWithCloud: true,
+    );
+
+    return note;
+  }
+
+  // FETCH USER
+  Future<DatabaseUser> getUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email: ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isEmpty) {
+      throw CouldNotFindUser();
+    } else {
+      return DatabaseUser.fromRow(results.first);
+    }
+  }
+
+  // CREATE USER
+  Future<DatabaseUser> createUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email: ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExists();
+    }
+
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toUpperCase(),
+    });
+
+    return DatabaseUser(
+      id: userId,
+      email: email,
+    );
+  }
+
+// DELETE USER
+  Future<void> deleteUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final deleteCount = await db.delete(
+      userTable,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deleteCount != 1) {
+      throw CouldNotDeleteUser();
+    }
+  }
+
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      return db;
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      await db.close();
+      _db = null;
+    }
+  }
 
   Future<void> open() async {
     if (_db != null) {
@@ -21,29 +177,10 @@ class NotesService {
       _db = db;
 
       // CREATE USER TABLE
-      const createUserTable = '''
-          CREATE TABLE IF NOT EXISTS "user" (
-          "id"	INTEGER NOT NULL,
-          "email"	TEXT NOT NULL UNIQUE,
-          PRIMARY KEY("id" AUTOINCREMENT)
-         ); ''';
-
       await db.execute(createUserTable);
 
       // CREATE NOTE TABLE
-      const createNoteTable = '''
-        CREATE TABLE IF NOT EXISTS "note" (
-          "id"	INTEGER NOT NULL,
-          "user_id"	INTEGER NOT NULL,
-          "text"	TEXT NOT NULL,
-          "is_synched_with_cloud"	INTEGER NOT NULL DEFAULT 0,
-          FOREIGN KEY("user_id") REFERENCES "user"("id"),
-          PRIMARY KEY("id" AUTOINCREMENT)
-        ); ''';
-
       await db.execute(createNoteTable);
-
-
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentsDirectory();
     }
@@ -113,3 +250,20 @@ const emailColumn = 'email';
 const userIdColumn = 'user_id';
 const textColumn = 'text';
 const isSynchedWithCloudColumn = 'is_synched_with_cloud_column';
+
+const createUserTable = '''
+          CREATE TABLE IF NOT EXISTS "user" (
+          "id"	INTEGER NOT NULL,
+          "email"	TEXT NOT NULL UNIQUE,
+          PRIMARY KEY("id" AUTOINCREMENT)
+         ); ''';
+
+const createNoteTable = '''
+        CREATE TABLE IF NOT EXISTS "note" (
+          "id"	INTEGER NOT NULL,
+          "user_id"	INTEGER NOT NULL,
+          "text"	TEXT NOT NULL,
+          "is_synched_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+          FOREIGN KEY("user_id") REFERENCES "user"("id"),
+          PRIMARY KEY("id" AUTOINCREMENT)
+        ); ''';
